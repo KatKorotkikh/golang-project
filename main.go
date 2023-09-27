a052409f258b2863fd0ee3fc9c5f25619a030547
Kat Korotkikh <kat.korotkikh@safetyculture.io>
2023-09-27 21:30:48 +1000

    previous homework

--- main.go
+++ main.go
@@ -5,11 +5,37 @@ import (
 	"net/http"
 )
 
+var validUserIDs = []string{
+	"user1",
+	"user2",
+	"user3",
+}
+
 func main() {
-	http.HandleFunc("/hello", HelloHandler)
+	http.HandleFunc("/dyn-user", HelloUser)
+	http.HandleFunc("/check-userid", CheckUserID)
 	http.ListenAndServe("0.0.0.0:8080", http.DefaultServeMux)
 }
 
-func HelloHandler(resp http.ResponseWriter, req *http.Request) {
-	fmt.Fprintln(resp, "Hello World!")
+func HelloUser(resp http.ResponseWriter, req *http.Request) {
+	id := req.URL.Query().Get("userid")
+	fmt.Fprintf(resp, "Hello, %s\n", id)
+}
+
+func CheckUserID(resp http.ResponseWriter, req *http.Request) {
+	id := req.URL.Query().Get("userid")
+	if isUserIdValid(id) {
+		fmt.Fprintln(resp, "Hello, user!")
+	} else {
+		http.Error(resp, "Bad user ID", http.StatusForbidden)
+	}
+}
+
+func isUserIdValid(id string) bool {
+	for _, validUser := range validUserIDs {
+		if id == validUser {
+			return true
+		}
+	}
+	return false
 }
