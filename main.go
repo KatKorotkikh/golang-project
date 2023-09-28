490bceae7b0e3db7362889e2385efbd7548310a8
Kat Korotkikh <kat.korotkikh@safetyculture.io>
2023-09-28 20:11:33 +1000

    week3 homework

--- main.go
+++ main.go
@@ -3,17 +3,16 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"sync"
 )
 
-var validUserIDs = []string{
-	"user1",
-	"user2",
-	"user3",
-}
+var validUserIDs sync.Map
 
 func main() {
 	http.HandleFunc("/dyn-user", HelloUser)
 	http.HandleFunc("/check-userid", CheckUserID)
+	http.HandleFunc("/add-user", AddUser)
+	http.HandleFunc("/delete-user", DeleteUser)
 	http.ListenAndServe("0.0.0.0:8080", http.DefaultServeMux)
 }
 
@@ -24,18 +23,34 @@ func HelloUser(resp http.ResponseWriter, req *http.Request) {
 
 func CheckUserID(resp http.ResponseWriter, req *http.Request) {
 	id := req.URL.Query().Get("userid")
-	if isUserIdValid(id) {
+	if isUserIDValid(id) {
 		fmt.Fprintln(resp, "Hello, user!")
 	} else {
 		http.Error(resp, "Bad user ID", http.StatusForbidden)
 	}
 }
 
-func isUserIdValid(id string) bool {
-	for _, validUser := range validUserIDs {
-		if id == validUser {
-			return true
-		}
+func AddUser(resp http.ResponseWriter, req *http.Request) {
+	id := req.URL.Query().Get("id")
+	if id != "" {
+		validUserIDs.Store(id, true)
+		fmt.Fprintf(resp, "User %s added successfully!\n", id)
+	} else {
+		http.Error(resp, "Invalid user ID", http.StatusBadRequest)
 	}
-	return false
+}
+
+func DeleteUser(resp http.ResponseWriter, req *http.Request) {
+	id := req.URL.Query().Get("id")
+	if id != "" {
+		validUserIDs.Delete(id)
+		fmt.Fprintf(resp, "User %s deleted successfully!\n", id)
+	} else {
+		http.Error(resp, "Invalid user ID", http.StatusBadRequest)
+	}
+}
+
+func isUserIDValid(id string) bool {
+	_, ok := validUserIDs.Load(id)
+	return ok
 }
